3d552f3c07b51f4d912c44efe11da313e76bf45c
David Mendoza Fernandez <dmendoza@car5s4.42barcelona.com>
2024-12-19 18:42:32 +0100

    strlcat and strlcpy

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,26 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/19 17:00:16 by dmendoza          #+#    #+#             */
-/*   Updated: 2024/12/19 18:35:54 by dmendoza         ###   ########.fr       */
+/*   Created: 2024/12/19 18:23:28 by dmendoza          #+#    #+#             */
+/*   Updated: 2024/12/19 18:36:21 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t			srclen;
+	size_t			dstlen;
 	unsigned int	i;
 
 	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
 	i = -1;
-	while (src[++i] && i < size)
-		dst[i] = src[i];
-	dst[i] = '\0';
-	return (srclen);
+	while (src[++i] && i < (size - dstlen - 1))
+		dst[i + dstlen] = src[i];
+	dst[i + dstlen] = '\0';
+	return (srclen + dstlen);
 }
