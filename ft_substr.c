652439198a6ec1752db616a1510415b1b3424ffc
David Mendoza Fernandez <dmendoza@cbr14s3.42barcelona.com>
2024-12-22 12:02:30 +0100

    substr norminette error corrected

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 11:25:47 by dmendoza          #+#    #+#             */
-/*   Updated: 2024/12/22 11:47:13 by dmendoza         ###   ########.fr       */
+/*   Updated: 2024/12/22 12:02:03 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*subs;
-	
+
 	subs = (char *)malloc(len);
 	if (subs == NULL)
 		return (NULL);
