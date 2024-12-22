de8d4b1a08f2adc39e2e648aad8bbd2645b348b0
David Mendoza Fernandez <dmendoza@cbr14s3.42barcelona.com>
2024-12-22 10:53:57 +0100

    calloc seems functional and bzero now works

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/20 11:57:53 by dmendoza          #+#    #+#             */
-/*   Updated: 2024/12/22 10:52:28 by dmendoza         ###   ########.fr       */
+/*   Created: 2024/12/20 16:05:23 by dmendoza          #+#    #+#             */
+/*   Updated: 2024/12/22 10:48:17 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void *s, size_t n)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	ft_memset(s, 0, n);
+	size_t	t_size;
+	void	*ptr;
+
+	if (nmemb > SIZE_MAX / size)
+		return (NULL);
+	t_size = nmemb * size;
+	ptr = (void *)malloc(t_size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, t_size);
+	return (ptr);
 }
