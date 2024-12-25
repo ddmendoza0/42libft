75648fce35b730abd75562eea63ef14ecb8f060b
David Mendoza Fernandez <dmendoza@cbr10s2.42barcelona.com>
2024-12-25 10:36:35 +0100

    putnbr fd now admits MINMAX_INT

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 15:50:29 by dmendoza          #+#    #+#             */
-/*   Updated: 2024/12/24 16:47:36 by dmendoza         ###   ########.fr       */
+/*   Updated: 2024/12/25 10:35:31 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	long	n_long;
+
+	n_long = (long)n;
 	if (n < 0)
 	{
 		write(fd, &"-", 1);
-		n = -n;
+		n_long *= -1;
 	}
-	if (n >= 10)
-		ft_putnbr_fd(n / 10, fd);
-	write(fd, &"0123456789"[n % 10], 1);
+	if (n_long >= 10)
+		ft_putnbr_fd(n_long / 10, fd);
+	write(fd, &"0123456789"[n_long % 10], 1);
 }
