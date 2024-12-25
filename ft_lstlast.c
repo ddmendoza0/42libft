24f992c8571b037a9a876e0dfb3e4a92599d57df
David Mendoza Fernandez <dmendoza@cbr10s2.42barcelona.com>
2024-12-25 11:59:47 +0100

    lstlast bonus function finished

---
+++ ft_lstlast.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/25 11:45:25 by dmendoza          #+#    #+#             */
+/*   Updated: 2024/12/25 11:58:14 by dmendoza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*curr;
+
+	curr = lst;
+	while (curr && curr->next)
+		curr = curr->next;
+	return (curr);
+}
