e233a82dc980fed135513a66113ee7a37356e2fc
David Mendoza Fernandez <dmendoza@cbr10s6.42barcelona.com>
2024-12-30 12:23:50 +0100

    susbtr returns now returning what should be returned :D

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dmendoza <dmendoza@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/22 11:25:47 by dmendoza          #+#    #+#             */
-/*   Updated: 2024/12/24 12:18:24 by dmendoza         ###   ########.fr       */
+/*   Updated: 2024/12/30 12:19:33 by dmendoza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	s_len = ft_strlen(s);
 	if (start >= s_len)
-		return ((char *)malloc(1));
-	subs = (char *)malloc((len + 1) * sizeof(char *));
+	{
+		subs = (char *)malloc(1);
+		*subs = '\0';
+		return (subs);
+	}
+	subs = (char *)malloc((len + 1) * sizeof(char));
 	if (subs == NULL)
 		return (NULL);
 	ft_strlcpy(subs, &s[start], len + 1);
